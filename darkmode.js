b0f8f0af70c028ea60076100903d9682ea9d2a50
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-12 15:58:33 -0600

    just need to figure out css to get colors to swap between light and dark

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,27 @@
-const darkmode = localStorage.getItem("darkMode");
+let darkMode = localStorage.getItem("darkMode");
 
+//Selecting the darkMode checkbox
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
+const enableDarkMode = () => {
+  document.body.classList.add("darkMode");
+  localStorage.setItem("darkMode", "enabled");
+};
+const disableDarkMode = () => {
+  document.body.classList.add("darkMode");
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
 darkModeToggle.addEventListener("click", () => {
-  console.log("testing");
+  //   console.log("testing");
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 });
