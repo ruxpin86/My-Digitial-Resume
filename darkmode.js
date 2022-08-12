7b9b864b73cd9cace6d251cca725b74aff94252a
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-12 15:30:45 -0600

    got toggle checkbox working properly and logging in the console

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,7 @@
 const darkmode = localStorage.getItem("darkMode");
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", () => {
+  console.log("testing");
+});
